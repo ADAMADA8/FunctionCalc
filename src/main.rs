704d429b586aca624eba559a45a8fa9a56914522
ADAMADA8 <adamk6344@gmail.com>
2026-02-10 02:05:21 +0400

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use anyhow::{Context, Result};
 use exmex::{parse, Express, FlatEx};
-use std::error::Error;
 use std::io::Write;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     println!("Добро пожаловать!");
     let func = read_function()?;
 
-    let [first_x, step_target, step_size] = read_parameters()?.try_into().unwrap();
+    let [first_x, step_target, step_size] = read_parameters()?.try_into()?;
     let max_iter = step_target + first_x;
 
     let mut step_length = step_target.to_string().len() + 1;
@@ -27,7 +27,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for i in first_x..max_iter {
         let x = (i * step_size) as f64;
-        let result = func.eval(&[x]).expect("Ошибка вычисления функции!");
+        let result = func.eval(&[x]).context("Ошибка вычисления функции!")?;
         let i = (i - first_x + 1) as f64;
         println!(
             "{}{}| {}{}| {}",
@@ -41,7 +41,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn read_function() -> Result<FlatEx<f64>, Box<dyn Error>> {
+fn read_function() -> Result<FlatEx<f64>> {
     println!("Введите функцию f(x)");
     println!("Пример: x + 10 * 4");
     print!("f(x) = ");
@@ -49,13 +49,13 @@ fn read_function() -> Result<FlatEx<f64>, Box<dyn Error>> {
     let mut func = String::new();
     std::io::stdin()
         .read_line(&mut func)
-        .expect("Ошибка чтения функции!");
+        .context("Ошибка чтения функции!")?;
     let func = func.trim();
-    let func = parse::<f64>(func).expect("Ошибка парсинга функции!");
+    let func = parse::<f64>(func).context("Ошибка парсинга функции!")?;
     Ok(func)
 }
 
-fn read_parameters() -> Result<(usize, usize, usize), Box<dyn Error>> {
+fn read_parameters() -> Result<(usize, usize, usize)> {
     println!("Введите параметры рекурсии (через пробел)");
     println!("<чему равен первый x> <количество шагов> <размер шага>");
     println!("Пример: 0 10 1");
@@ -64,12 +64,13 @@ fn read_parameters() -> Result<(usize, usize, usize), Box<dyn Error>> {
     let mut params = String::new();
     std::io::stdin()
         .read_line(&mut params)
-        .expect("Ошибка чтения параметров!");
+        .context("Ошибка чтения параметров!")?;
 
     let params: Vec<usize> = params
         .trim()
         .split_whitespace()
-        .map(|s| s.parse().expect("Ошибка парсинга параметров!"))
-        .collect();
+        .map(|s| s.parse())
+        .collect::<Result<Vec<_>, _>>()
+        .context("Ошибка парсинга параметров!")?;
     Ok((params[0], params[1], params[2]))
 }
